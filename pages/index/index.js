26b9ac8d11525ccaa7da5265d8189eb3924a4f03
WaitForYou <1342385875@qq.com>
2017-10-19 15:04:32 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,74 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    payWayData: 0,
+    numItem: [1, 5, 10, 15],
+    timeItem: [1, 2, 3, 5]
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  //按时，按次的事件处理
+  payWay: function(e) {
+    console.log(e)
+    if (e.target.id == 'numPay'){
+      this.setData({
+        payWayData: 1
+      })
+    } else if (e.target.id == 'timePay'){
+      this.setData({
+        payWayData: 2
+      })
+    }else{
+      this.setData({
+        payWayData: 0
+      })
+    }
+  }
+})
