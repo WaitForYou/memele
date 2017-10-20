fd12f7a97a0a190eb31e15f33f3870635a57ed77
WaitForYou <1342385875@qq.com>
2017-10-20 15:50:58 +0800

    完成交互

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,15 +8,30 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    payWayData: 0,
-    numItem: [1, 5, 10, 15],
-    timeItem: [1, 2, 3, 5]
+    payWayData: 0,//是按次还是按时购买，1按次，2按时
+    numItem: [1, 5, 10, 15],//按次的购买按钮选择
+    timeItem: [1, 2, 3, 5],//按时的购买按钮选择
+    COSTEACHTIME: 0.5,//按次购买的单价
+    COSTEACHHOUR: 15,//按时购买的单价
+    hlNumValue: 1,
+    hlTimeValue: 1,
+    inputValue: '',
+    cost: 0,
+    payStage: 0,//0是关闭，1是成功，2是失败
   },
   //事件处理函数
-  bindViewTap: function() {
+  backToHomePageFn: function() {
+    this.setData({
+      payStage: 0
+    });
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '../index/index'
     })
+  },
+  closeTipsFn: function(){
+    this.setData({
+      payStage: 0
+    });
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
@@ -70,5 +85,61 @@ Page({
         payWayData: 0
       })
     }
+  },
+  //选择某个次数或者某个时长
+  chooseFn: function(e) {
+    console.log(e)
+    this.setData({
+      inputValue: ""
+    });
+    if (this.data.payWayData == 1){
+      this.setData({
+        hlNumValue: e.target.dataset.choseResult
+      });
+    } else if (this.data.payWayData == 2){
+      this.setData({
+        hlTimeValue: e.target.dataset.choseResult
+      });
+    }
+  },
+  //获取input框的值
+  bindKeyInput: function(e){
+    console.log(e)
+    this.setData({
+      inputValue: e.detail.value
+    });
+  },
+  //支付按钮事件
+  payMoneyNowFn: function(e){
+    let cost = 0,
+        times = 0;
+
+    if(this.data.payWayData == 1){//按次数购买
+      times = (this.data.inputValue) ? this.data.inputValue : this.data.hlNumValue;//如果输入框不为空，则用输入框的值
+      cost = times * this.data.COSTEACHTIME;
+    }else{
+      times = (this.data.inputValue) ? this.data.inputValue : this.data.hlTimeValue;
+      cost = times * this.data.COSTEACHHOUR;
+    }
+
+    this.data.cost = cost;
+    console.log(this.data.cost)
+    this.setData({
+      payStage: 1
+    });
+
+    // wx.requestPayment({
+    //   'timeStamp': '',
+    //   'nonceStr': '',
+    //   'package': '',
+    //   'signType': 'MD5',
+    //   'paySign': '',
+    //   'success': function (res) {
+    //     console.log(1)
+    //   },
+    //   'fail': function (res) {
+    //     console.log(2)
+    //   }
+    // })
   }
 })
